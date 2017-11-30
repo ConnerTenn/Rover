5d937726169d2280f97f37d37c8506a5604c2f6f
ConnerTenn@Laptop <connertenn@gmail.com>
2017-11-30 18:00:05 -0500

    no message

--- LIDAR/LIDAR_00/LMS1xx.h
+++ LIDAR/LIDAR_00/LMS1xx.h
@@ -1,9 +1,47 @@
 
+struct ScanConfig;
 class LMS1xx;
 
 #pragma once
 
 #include "LIDAR.h"
+
+struct ScanConfig
+{
+	int Frequency; // 1/100 Hz
+	int Resolution; // 1/10000 degree
+	int StartAngle; // 1/10000 degree
+	int StopAngle; // 1/10000 degree
+};
+
+struct ScanDataConfig
+{
+	int OutputChannel; //Defines which output channel is activated.
+	bool Remission; //Defines whether remission values are output.
+	int Resolution; //Defines whether the remission values are output with 8-bit or 16bit resolution.
+	int Encoder; //Defines which output channel is activated.
+	bool Position; //Defines whether position values are output.
+	bool DeviceName; //Determines whether the device name is to be output.
+	bool Timestamp;
+	int OutputInterval; //Defines which scan is output.  01 every scan. 02 every 2nd scan. 50000 every 50000th scan
+};
+
+struct ScanData
+{
+	int DistSize1; //Number of samples in dist1.
+	uint16_t Dist1[1082]; //Radial distance for the first reflected pulse
+
+	
+	int DistSize2; //Number of samples in dist2.
+	uint16_t Dist2[1082]; //Radial distance for the second reflected pulse
+
+	int RssiSize1; //Number of samples in Rssi1.
+	uint16_t Rssi1[1082]; //Remission values for the first reflected pulse
+
+	
+	int RssiSize2; //Number of samples in rssi2.
+	uint16_t Rssi2[1082]; //Remission values for the second reflected pulse
+};
 
 class LMS1xx
 {
